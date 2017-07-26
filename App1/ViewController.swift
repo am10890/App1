19d2ba895ba2526a599b39f7b8fed518ace1f4a8
AlanAlan <alanalan@Cybercrimes-iMac.local>
2017-07-26 11:42:36 +0100

    Change to 19

--- App1/ViewController.swift
+++ App1/ViewController.swift
@@ -3,16 +3,41 @@
 //  App1
 //
 //  Created by AlanAlan on 24/07/2017.
-//  Copyright © 2017 AlanAlan. All rights reserved.
+//  Copyright © 2017 AlanAlan. All rights reserved...change
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Label1: UILabel!
+    
+    var tapCount = 0
+    
+    @IBAction func PressOne(_ sender: Any) {
+        
+       tapCount = tapCount + 1
+    
+        if tapCount > 19 {
+            Label1.text = "STOP PRESSING THE BLOODY BUTTON!!"
+        }
+        
+    }
+    
+    
+    @IBAction func PressTwo(_ sender: Any) {
+        
+        Label1.text = "TWO!"
+        
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
